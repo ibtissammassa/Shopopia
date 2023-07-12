16dcdc73928e2bd7496594b243aadec75ef79240
Ibtissam Massa <75497523+ibtissammassa@users.noreply.github.com>
2023-07-12 14:53:37 +0100

    header top

--- pages/index.vue
+++ pages/index.vue
@@ -6,17 +6,6 @@
 
 <script>
   export default {
-    data(){
-      return{
-        phone: this.$settings.header_top.phone,
-        message: this.$settings.header_top.message,
-        backgroundcolor : this.$settings.header_top.header_color.background,
-        color: this.$settings.header_top.header_color.color,
-        isPhone: this.$settings.header_top.show.phone,
-        isMessage: this.$settings.header_top.show.message,
-        isLanguage: this.$settings.header_top.show.language,
-        isCurrency: this.$settings.header_top.show.currency
-      }
-    }
+    
 }
 </script>
