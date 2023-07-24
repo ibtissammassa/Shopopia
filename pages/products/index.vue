d98dc3408513d191b36d935cb92d3a1b8c387ac4
Ibtissam Massa <75497523+ibtissammassa@users.noreply.github.com>
2023-07-24 15:28:32 +0100

    search

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -4,7 +4,7 @@
         <h2 class="text-2xl font-bold pl-6 mt-5">{{ title }}</h2>
         <div class="grid grid-cols-4">
             <productCard v-for="item in products" :key="item.id" :item="item"/>
-        </div>{{ $store.state.cart }}
+        </div>
     </div>
   
 </template>
@@ -15,16 +15,29 @@ export default {
         return{
             title: this.$settings.shop.title,
             products:[],
-            categories:[]
+            categories:[],
         }
     },
     async fetch(){
         const filter = { status: 'PUBLISH'};
         this.products = await this.getProducts(filter);
     },
+    watch:{
+        async "$route.query.search"(){
+            const filter = { status: 'PUBLISH'};
+            this.products = await this.getProducts(filter);
+        }
+    },
     methods: {
         async getProducts(filter){
           try{
+              const search = this.$route.query.search;
+              console.log(search);
+            if(search){
+                const params = { search: search, page: 1, limit: 5 }
+                const { data } = await this.$storeino.products.search(params);
+                return data.results;
+            }
             const { data } = await this.$storeino.products.search(filter)
             return data.results
           }catch(e){
