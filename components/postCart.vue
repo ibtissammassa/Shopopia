6c8683b8a8f8e6952cb04a62acb8b785c45f769e
ibtissammassa <bettysama16@gmail.com>
2023-07-31 10:59:09 +0100

    blog

--- components/postCart.vue
+++ components/postCart.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
-    
-  </div>
+  <nuxt-link :to="`/post/${item.slug}`" class="border-slate-100 border-2 rounded-xl cursor-pointer flex flex-col">
+    <div class="w-full overflow-hidden rounded-xl h-2/3">
+      <img class="zoom object-cover w-full h-full" :src="item.image.url" alt="">
+    </div>
+    <div class="p-3 flex flex-col gap-y-2 ">
+      <h3 class="font-bold text-lg">{{ item.title }}</h3>
+      <p class="text-gray-700 text-sm">{{ item.excerpt }}</p>
+    </div>
+  </nuxt-link>
 </template>
 
 <script>
 export default {
-
+  props:['item']
 }
 </script>
 
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="px-10 py-8">
     <h3 class="text-2xl pb-3 font-bold">Our Blog :</h3>
-    <hr>{{ items }}
-    <div>
-        
-    </div>
-    <div v-if="items">
-        
+    <hr>
+    <div v-if="items" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 gap-4">
+        <postCart v-for="item in items" :key="item.id" :item="item"/>
     </div>
     <loading v-else/>
     <fromShop/>
@@ -14,7 +11,9 @@
 </template>
 
 <script>
+import ProductCard from '~/components/ProductCard.vue';
 export default {
+  components: { ProductCard },
     data(){
         return{
             items: null
@@ -22,11 +21,10 @@ export default {
     },
     async fetch(){
         try{
-                const { data } = await this.$storeino.pages.search({ type: 'POST' });
-                console.log(data);
+                const params = { type: 'POST' };
+                const { data } = await this.$storeino.pages.search(params);
                 this.items = [];
                 this.items = data.results;
-                console.log(this.items);
             }catch(e){
                 console.log({e});
             }
