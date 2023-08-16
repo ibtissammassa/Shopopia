8097651e1bbbb80e843ec794634a402f7696372e
ibtissammassa <bettysama16@gmail.com>
2023-08-17 00:28:15 +0100

    post

--- components/postCart.vue
+++ components/postCart.vue
@@ -3,7 +3,7 @@
     <div class="w-full overflow-hidden rounded-xl h-2/3">
       <img class="zoom object-cover w-full h-full" :src="item.image.url" alt="">
     </div>
-    <div class="p-3 flex flex-col gap-y-2">
+    <div class="py-3 flex flex-col gap-y-2">
       <h3 class="font-bold text-lg">{{ item.title }}</h3>
       <p class="text-gray-700 text-sm">{{ item.excerpt }}</p>
     </div>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -26,7 +26,7 @@
             <fromShop class="py-2" v-if="showProducts"/>
         </div>
         <div class="lg:border-l px-6 lg:w-full">
-            <div :class="!sidebar ? 'justify-between' : 'justify-start'" class="flex bg-slate-100 rounded-md p-3 px-5 my-5 mx-3">
+            <div :class="!sidebar ? 'justify-between' : 'justify-end'" class="flex bg-slate-100 rounded-md p-3 px-5 my-5 mx-3">
                 <button v-if="!sidebar" class="text-sm flex items-center gap-x-1.5" @click="sidebar=true">
                     <svg class="w-4 h-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path d="M0 0h24v24H0z" fill="none"/><path d="M10 14L4 5V3h16v2l-6 9v6l-4 2z"/></g></svg>
                     <p>Filter Posts</p>
